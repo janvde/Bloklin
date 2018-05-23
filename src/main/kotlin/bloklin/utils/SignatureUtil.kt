d36ad0adfb807cd1c56757fbc0d74171414e5d0a
Jan van den Ende <jan@getroadmap.com>
2018-05-23 22:39:12 +0200

    verify a ECDSA signature

--- src/main/kotlin/bloklin/utils/SignatureUtil.kt
+++ src/main/kotlin/bloklin/utils/SignatureUtil.kt
@@ -1,6 +1,7 @@
 package bloklin.utils
 
 import java.security.PrivateKey
+import java.security.PublicKey
 import java.security.Signature
 
 object SignatureUtil {
@@ -9,10 +10,20 @@ object SignatureUtil {
      * sign a message with a given privatekey using a given algorithm and provider
      */
     private fun sign(privateKey: PrivateKey, message: String, algorithm: String, provider: String): ByteArray {
-        val signature = Signature.getInstance("algorithm", provider)
+        val signature = Signature.getInstance(algorithm, provider)
         signature.initSign(privateKey)
         signature.update(message.toByteArray());
         return signature.sign()
+    }
+
+    /**
+     * verify a signature using a given algorithm and provider
+     */
+    private fun verify(publicKey: PublicKey, data: String, signature: ByteArray, algorithm: String, provider: String): Boolean {
+        val sig = Signature.getInstance(algorithm, provider)
+        sig.initVerify(publicKey)
+        sig.update(data.toByteArray());
+        return sig.verify(signature)
     }
 
     /**
@@ -20,6 +31,14 @@ object SignatureUtil {
      */
     fun signWithECDSA(privateKey: PrivateKey, message: String): ByteArray {
         return sign(privateKey, message, "ECDSA", "BC")
+    }
+
+
+    /**
+     * verify a Elliptic Curve Digital Signature Algorithm signature
+     */
+    fun verifyECDSA(publicKey: PublicKey, data: String, signature: ByteArray): Boolean {
+        return verify(publicKey, data, signature, "ECDSA", "BC")
     }
 
 
